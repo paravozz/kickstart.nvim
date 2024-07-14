c1ce4f3d67ae76fd5019341797488c9a8a63829e
paravozz <prvz-work@outlook.com>
2024-07-14 01:49:37 +0100

    add lazygit term

--- init.lua
+++ init.lua
@@ -173,7 +173,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -873,6 +873,7 @@ require('lazy').setup({
     lazy = false,
     config = function()
       vim.keymap.set('n', '<leader>T', ':ToggleTerm name=main<CR>', { silent = true, desc = 'Toggle [T]erminal' })
+      vim.keymap.set('n', '<leader>GT', ':TermExec cmd="lazygit" name=gitterm direction=float<CR>', { silent = true, desc = 'Toggle [G]it [T]erminal' })
 
       require('toggleterm').setup {
         size = 15,
